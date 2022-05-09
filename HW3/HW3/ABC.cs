09d1683d59dc4f5da231e5a65f9330a4e631a680
VilkovEgor <100317011+VilkovEgor@users.noreply.github.com>
2022-05-09 12:33:59 +0300

    last

--- HW3/HW3/ABC.cs
+++ HW3/HW3/ABC.cs
@@ -8,9 +8,98 @@ namespace HW3
 {
     public class ABC
     {
-        public void q(string w)
+        public string ABC1(string[] array)
         {
-            Console.WriteLine (w+"abc");
+            string abc = " ";
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                switch (array[i])
+                {
+                    case "1":
+                        abc += "A" + " ";
+                        break;
+                    case "2":
+                        abc += "b" + " ";
+                        break;
+                    case "3":
+                        abc += "c" + " ";
+                        break;
+                    case "4":
+                        abc += "D" + " ";
+                        break;
+                    case "5":
+                        abc += "e" + " ";
+                        break;
+                    case "6":
+                        abc += "f" + " ";
+                        break;
+                    case "7":
+                        abc += "g" + " ";
+                        break;
+                    case "8":
+                        abc += "H" + " ";
+                        break;
+                    case "9":
+                        abc += "I" + " ";
+                        break;
+                    case "10":
+                        abc += "J" + " ";
+                        break;
+                    case "11":
+                        abc += "k" + " ";
+                        break;
+                    case "12":
+                        abc += "l" + " ";
+                        break;
+                    case "13":
+                        abc += "m" + " ";
+                        break;
+                    case "14":
+                        abc += "n" + " ";
+                        break;
+                    case "15":
+                        abc += "o" + " ";
+                        break;
+                    case "16":
+                        abc += "p" + " ";
+                        break;
+                    case "17":
+                        abc += "q" + " ";
+                        break;
+                    case "18":
+                        abc += "r" + " ";
+                        break;
+                    case "19":
+                        abc += "s" + " ";
+                        break;
+                    case "20":
+                        abc += "t" + " ";
+                        break;
+                    case "21":
+                        abc += "u" + " ";
+                        break;
+                    case "22":
+                        abc += "v" + " ";
+                        break;
+                    case "23":
+                        abc += "w" + " ";
+                        break;
+                    case "24":
+                        abc += "x" + " ";
+                        break;
+                    case "25":
+                        abc += "y" + " ";
+                        break;
+                    case "26":
+                        abc += "z" + " ";
+                        break;
+
+                }
+
+
+            }
+            return abc;
         }
     }
         
--- HW3/HW3/Program.cs
+++ HW3/HW3/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using HW3;
+
+ABC eee = new ABC();
 int[] a = new int[Convert.ToInt32(Console.ReadLine())];
 string b1 = "";
 string c1 = "";
@@ -35,106 +37,14 @@ for (int i = 0; i < even.Length; i++)
     Console.Write($"{even[i]} ");
 }
 Console.WriteLine();
-string ABC(string[] array )
-{
-    string abc = " ";
-    for(int i=0; i<array.Length; i++)
-    {
-        
-        switch (array[i])
-        {
-            case "1":
-                abc += "A" + " ";
-                break;
-            case "2":
-                abc += "b" + " ";
-                break;
-            case "3":
-                abc += "c" + " ";
-                break;
-            case "4":
-                abc += "D" + " ";
-                break;
-            case "5":
-                abc += "e" + " ";
-                break;
-            case "6":
-                abc += "f" + " ";
-                break;
-            case "7":
-                abc += "g" + " ";
-                break;
-            case "8":
-                abc += "H" + " ";
-                break;
-            case "9":
-                abc += "I" + " ";
-                break;
-            case "10":
-                abc += "J" + " ";
-                break;
-            case "11":
-                abc += "k" + " ";
-                break;
-            case "12":
-                abc += "l" + " ";
-                break;
-            case "13":
-                abc += "m" + " ";
-                break;
-            case "14":
-                abc += "n" + " ";
-                break;
-            case "15":
-                abc += "o" + " ";
-                break;
-            case "16":
-                abc += "p" + " ";
-                break;
-            case "17":
-                abc += "q" + " ";
-                break;
-            case "18":
-                abc += "r" + " ";
-                break;
-            case "19":
-                abc += "s" + " ";
-                break;
-            case "20":
-                abc += "t" + " ";
-                break;
-            case "21":
-                abc += "u" + " ";
-                break;
-            case "22":
-                abc += "v" + " ";
-                break;
-            case "23":
-                abc += "w" + " ";
-                break;
-            case "24":
-                abc += "x" + " ";
-                break;
-            case "25":
-                abc += "y" + " ";
-                break;
-            case "26":
-                abc += "z" + " ";
-                break;
-           
-        }
-        
-       
-    }
-    return abc;
-}
+
 Console.WriteLine("Change odd");
-Console.WriteLine(ABC(odd));
+Console.WriteLine(eee.ABC1(odd));
 Console.WriteLine("Change even");
-Console.WriteLine(ABC(even));
+Console.WriteLine(eee.ABC1(even));
 Console.WriteLine();
-string[] ABCodd = ABC(odd).Split(new char[] { ' ' });
-string[] ABCeven = ABC(even).Split(new char[] { ' ' });
+string[] ABCodd = eee.ABC1(odd).Split(new char[] { ' ' });
+string[] ABCeven = eee.ABC1(even).Split(new char[] { ' ' });
 int CountBigodd=0;
 int CountBigeven=0;
 for (int i = 0; i < ABCodd.Length; i++)
@@ -199,7 +109,7 @@ if (CountBigodd> CountBigeven)
     Console.Write("odd:");
     
     
-        Console.Write(ABC(odd));
+        Console.Write(eee.ABC1(odd));
     
         
        
@@ -208,6 +118,6 @@ else
 {
     Console.Write("even:");
     
-        Console.Write(ABC(even));
+        Console.Write(eee.ABC1(even));
     
 }
